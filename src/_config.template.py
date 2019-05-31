6130b1310f372813275c6c903d45e04a93c07dfd
benjamin holmes <bh0085@hb9.csail.mit.edu>
2019-05-31 16:13:01 -0400

    adds config template changes

--- src/_config.template.py
+++ src/_config.template.py
@@ -2,7 +2,7 @@ import os, re, sys
 import pandas as pd
 
 
-PRJ_DIR = '/cluster/bh0085/prj/reduced/reduced_sm/'
+PRJ_DIR = '/cluster/bh0085/prj/reduced_all/reduced_sm/'
 
 
 #######################################################
@@ -24,6 +24,40 @@ SEQUENCING_READS_META = {"reads_28": {"reads_place":"/cluster/bh0085/shortreads/
                 "reads_502":{"reads_place":'/cluster/bh0085/shortreads/190502Gif',
                             "lib_file":"../libs/smallmol2 sequencing directory contents - lib502.csv"}
                }
+
+
+SEQUENCING_INFO = pd.DataFrame()
+for nm,vals in SEQUENCING_READS_META.items():
+    #read sequencing maps and experimental design files
+    reads_place = vals["reads_place"]
+    lib_file = vals["lib_file"]
+    reads_files = os.listdir(reads_place)
+    info = pd.read_csv(lib_file) 
+
+    #add metadata to sequencing info DF
+    info = pd.concat([info,
+                      info.apply(
+        lambda x:pd.Series( re.compile("(?P<Name>D[^\s]+) : (?P<Description>[^()]*)\((?P<Index>[^)]*)\)")
+                           .search(x["code"]).groupdict()),axis=1)]
+                     ,axis=1)
+    info["nm"] = nm
+    info["reads_place"] = reads_place
+    reads_files =os.listdir(reads_place)
+    
+    info["reads_location"] = info.Name.apply(
+        lambda x: [e for e in reads_files if x in e]).apply(
+            lambda x:os.path.join(reads_place,x[0]) if len(x)>0 
+        else None)
+    
+    #find fastq files using sequencing info DF
+    if False in set(pd.notna(info.reads_location)):
+        raise Exception(f"missing reads data file for {nm}")
+    info["fastq"] = info.apply(lambda x:[fn for fn in os.listdir(x.reads_location) if ~(pd.isna(x.reads_location)) & ( fn[-5:]=="fastq")][0],axis=1)
+    info["fastq_path"] = info["reads_location"] +"/"+ info["fastq"]
+    SEQUENCING_INFO = SEQUENCING_INFO.append(info)
+     
+ 
+
 
 REDUCED_LIB = pd.read_csv("~/data/reduced_lib_design.csv")
 REDUCED_LIB["Name"] = REDUCED_LIB["Identifier number"].apply(lambda x: int(x)) - 1
