e74a6f434a29353ea9f03360ecdeec0ca466addd
benjamin holmes <bh0085@hb9.csail.mit.edu>
2019-06-03 01:40:07 -0400

    adds filtering for unrelated sequening runs

--- src/_config.template.py
+++ src/_config.template.py
@@ -2,17 +2,13 @@ import os, re, sys
 import pandas as pd
 import datetime
 
-
-PRJ_DIR = '/cluster/bh0085/prj/reduced_all/reduced_liver/'
-PRJ_NAME = 'REDUCEDLIB_LIVER_May2019'
-
-
+PRJ_DIR = '/cluster/bh0085/prj/reduced_all/reduced_sm/'
+PRJ_NAME = 'REDUCEDLIB_SMALLMOL_May2019'
 
 today = datetime.date.today()
 DATESTR = "{0:02}{0:02}".format(today.month,today.day)
 
-
-######################################################
+#######################################################
 # Note: Directories should end in / always
 #######################################################
 SRC_DIR = os.path.join(PRJ_DIR , 'src/')
@@ -22,13 +18,16 @@ QSUBS_DIR = os.path.join( PRJ_DIR + 'qsubs/')
 FIGS_PLACE = os.path.join(PRJ_DIR , 'figs/')
 
 
-
-
 #dictionary keyed by sequencing group names, containing:
 #  "reads_place" (with folders for each sequencing lane)
 #  "lib_file" (having names and descriptions associated with each sequencing lane)
-SEQUENCING_READS_META = {"reads_all": {"reads_place":"/cluster/bh0085/shortreads/190510Gif",
-                             "lib_file":"../libs/reduced_liver sequencing reads - lib510.csv"}
+SEQUENCING_READS_META = {
+    #"reads_28": {"reads_place":"/cluster/bh0085/shortreads/190328Gif",
+                #              "lib_file":"../libs/smallmol2 sequencing directory contents - lib28.csv"},
+                # "reads_29":  {"reads_place":"/cluster/bh0085/shortreads/190329Gif",
+                #               "lib_file":"../libs/smallmol2 sequencing directory contents - lib29.csv"},
+                 "reads_502":{"reads_place":'/cluster/bh0085/shortreads/190502Gif',
+                            "lib_file":"../libs/smallmol2 sequencing directory contents - lib502.csv"}
                }
 
 
@@ -63,7 +62,10 @@ for nm,vals in SEQUENCING_READS_META.items():
     SEQUENCING_INFO = SEQUENCING_INFO.append(info)
      
  
-
+SEQUENCING_INFO["Description"] = SEQUENCING_INFO.apply(lambda x:re.compile(": (.*)\(.*\)").search(x.code).groups()[0].strip(),axis=1)
+SEQUENCING_INFO = SEQUENCING_INFO.loc[18:77]
+SEQUENCING_INFO["drug_name"] = SEQUENCING_INFO.Description.str.slice(0,-2)
+SEQUENCING_INFO["replicate"] = SEQUENCING_INFO.Description.str.slice(-1)
 
 REDUCED_LIB = pd.read_csv("/cluster/bh0085/data/reduced_lib_design.csv")
 REDUCED_LIB["Name"] = REDUCED_LIB["Identifier number"].apply(lambda x: int(x)) - 1
