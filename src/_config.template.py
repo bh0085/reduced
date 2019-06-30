afb0bed0e5b17f81c11edd23cb96c8f1f402fe73
benjamin holmes <bh0085@foxp1.csail.mit.edu>
2019-06-30 13:25:49 -0400

    removes reads directory from config file

--- src/_config.template.py
+++ src/_config.template.py
@@ -2,8 +2,8 @@ import os, re, sys
 import pandas as pd
 import datetime
 
-PRJ_DIR = '/cluster/bh0085/prj/reduced_all/doublereduced2'
-PRJ_NAME = 'REDUCEDLIB_DOUBLEREDUCED_June2019'
+PRJ_DIR = '/cluster/bh0085/prj/reduced_all/heysol'
+PRJ_NAME = 'REDUCEDLIB_heysol_June2019'
 
 today = datetime.date.today()
 DATESTR = "{0:02}{0:02}".format(today.month,today.day)
@@ -17,8 +17,7 @@ OUT_PLACE = os.path.join(PRJ_DIR , 'out/')
 QSUBS_DIR = os.path.join( PRJ_DIR + 'qsubs/')
 FIGS_PLACE = os.path.join(PRJ_DIR , 'figs/')
 LOGS_DIR = os.path.join(PRJ_DIR, 'logs/')
-READS_DIR="/cluster/bh0085/shortreads/FC_04530/Unaligned_1234_PF_mm1/Data/Project_richsherwood/"
 
 
 #parse sequencing data information 
-from a0_library_loaders_doublereduced_subproj import SEQUENCING_INFO, LIBRARY_DF
+from a0_library_loaders_heysol_subproj import SEQUENCING_INFO, LIBRARY_DF
